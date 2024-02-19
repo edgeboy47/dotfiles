fd739ea965d386bb6d3ad70360ab214986a63b44
Darrion Mills <dmills1994@gmail.com>
2024-02-18 22:01:17 -0400

    Add powershell profile

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+
+Set-Alias lvim 'C:\Users\Dj\.local\bin\lvim.ps1'
+oh-my-posh init pwsh --config 'C:\Users\Dj\AppData\Local\Programs\oh-my-posh\themes\catppuccin_macchiato.omp.json'| Invoke-Expression
+
+Import-Module posh-git
+Import-Module -Name Terminal-Icons
